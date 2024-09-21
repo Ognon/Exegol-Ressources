09618d9483d32629ff76db995eedc89f217ec3c4
Ognon <146648858+Ognon@users.noreply.github.com>
2024-09-21 13:12:58 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ pip3 install wsgidav
 pip install cheroot
 pip3 install pyftpdlib
 
-apt-get install snmp-mibs-downloader
+apt install snmp-mibs-downloader
 apt install ripgrep
 
 echo "PermitRootLogin prohibit-password" >> /etc/ssh/sshd_config
@@ -42,6 +42,14 @@ sed -i 's/"#"/"%{$fg[green]%}➜ %{$reset_color%}"/g' /opt/.exegol_shells_rc
 sed -i 's/"%m"/"${HOSTNAME#exegol-}"/g' /opt/.exegol_shells_rc
 sed -i 's/(%Z)//g' ~/.zshrc
 
+#Install Sublime text
+
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+
+# Update + finalisation des install
+apt update
+apt install sublime-text
 
 # On supprime l'install.sh
 rm /workspace/install.sh
